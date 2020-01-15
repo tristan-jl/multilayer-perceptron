6ca33f0fafd2e944d98e02bd443b6724d8a6ff73
Tristan Laurens <tristanlaurens@hotmail.com>
2020-01-15 16:00:27 +0000

    fix dynamic plot update and output training weights

--- src/multilayer-perceptron.py
+++ src/multilayer-perceptron.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from IPython.display import clear_output
 from Network.Layers import Dense, ReLU
 from Network.utils import load_dataset, plot_sample, iterate_minibatches, predict, train
 
@@ -17,29 +16,35 @@ def main():
         Dense(200, 10),
     ]
 
-    plot_sample(train_data, train_labels)
-    print(test_labels)
+    # plot_sample(train_data, train_labels)
+    # print(test_labels)
 
     train_log = []
     val_log = []
+    plt.show()
     for epoch in range(25):
         for x_batch, y_batch in iterate_minibatches(
             train_data, train_labels, batchsize=32, shuffle=True
         ):
             train(network, x_batch, y_batch)
 
-        train_log.append(np.mean(predict(network, train_data) == test_data))
+        train_log.append(np.mean(predict(network, train_data) == train_labels))
         val_log.append(np.mean(predict(network, test_data) == test_labels))
 
-        clear_output()
-        print("Epoch", epoch)
+        print("Epoch", epoch + 1)
         print("Train accuracy:", train_log[-1])
         print("Val accuracy:", val_log[-1])
+
         plt.plot(train_log, label="train accuracy")
         plt.plot(val_log, label="val accuracy")
-        plt.legend(loc="best")
-        plt.grid()
-        plt.show()
+        # plt.legend(loc="best")
+        plt.draw()
+        plt.pause(0.001)
+
+    np.save("../data/weights/0.npy", network[0].weights)
+    np.save("../data/weights/2.npy", network[2].weights)
+    np.save("../data/weights/4.npy", network[4].weights)
+    plt.show()
 
 
 if __name__ == "__main__":

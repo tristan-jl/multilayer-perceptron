2e017246240347830228a721262684b890002f97
Tristan Laurens <tlaurens@deloitte.co.uk>
2020-01-17 10:47:38 +0000

    update project structure and add flask app

---
+++ network-creation/src/train.py
@@ -0,0 +1,48 @@
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+from Network.Layers import Dense, ReLU
+from Network.utils import load_dataset, iterate_minibatches, predict, train
+
+plt.style.use("dark_background")
+
+
+def main():
+    train_data, train_labels, test_data, test_labels = load_dataset(flatten=True)
+    network = [
+        Dense(train_data.shape[1], 100),
+        ReLU(),
+        Dense(100, 200),
+        ReLU(),
+        Dense(200, 10),
+    ]
+
+    train_log = []
+    val_log = []
+    plt.show()
+    for epoch in range(25):
+        for x_batch, y_batch in iterate_minibatches(
+            train_data, train_labels, batchsize=32, shuffle=True
+        ):
+            train(network, x_batch, y_batch)
+
+        train_log.append(np.mean(predict(network, train_data) == train_labels))
+        val_log.append(np.mean(predict(network, test_data) == test_labels))
+
+        print("Epoch", epoch + 1)
+        print("Train accuracy:", train_log[-1])
+        print("Val accuracy:", val_log[-1])
+
+        plt.plot(train_log, label="train accuracy")
+        plt.plot(val_log, label="val accuracy")
+        plt.draw()
+        plt.pause(0.001)
+
+    pickle.dump({"network": network}, open("../../dist/network_file" + ".p", "wb"))
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()

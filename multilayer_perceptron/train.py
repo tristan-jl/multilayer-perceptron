54561d0ecfa845c64914bf7ada2a49ce6cfce762
Tristan Laurens <tristanlaurens@hotmail.com>
2020-01-24 16:36:21 +0000

    Recreate project structure

--- multilayer_perceptron/train.py
+++ multilayer_perceptron/train.py
@@ -3,21 +3,22 @@ import pickle
 import matplotlib.pyplot as plt
 import numpy as np
 
-from Network.Layers import Dense, ReLU
-from Network.utils import load_dataset, iterate_minibatches, predict, train
+from Network.layers import Dense, ReLU
+from Network.network import Network
+from Network.utils import load_dataset, iterate_minibatches
 
 plt.style.use("dark_background")
 
 
 def main():
     train_data, train_labels, test_data, test_labels = load_dataset(flatten=True)
-    network = [
+    network = Network([
         Dense(train_data.shape[1], 100),
         ReLU(),
         Dense(100, 200),
         ReLU(),
         Dense(200, 10),
-    ]
+    ])
 
     train_log = []
     val_log = []
@@ -26,10 +27,10 @@ def main():
         for x_batch, y_batch in iterate_minibatches(
             train_data, train_labels, batchsize=32, shuffle=True
         ):
-            train(network, x_batch, y_batch)
+            network.train(x_batch, y_batch)
 
-        train_log.append(np.mean(predict(network, train_data) == train_labels))
-        val_log.append(np.mean(predict(network, test_data) == test_labels))
+        train_log.append(np.mean(network.predict(train_data) == train_labels))
+        val_log.append(np.mean(network.predict(test_data) == test_labels))
 
         print("Epoch", epoch + 1)
         print("Train accuracy:", train_log[-1])
@@ -40,7 +41,7 @@ def main():
         plt.draw()
         plt.pause(0.001)
 
-    pickle.dump({"network": network}, open("../../dist/network_file" + ".p", "wb"))
+    pickle.dump({"network": network}, open("../dist/network_file" + ".p", "wb"))
     plt.show()
 
 
